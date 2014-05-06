afe76f96bb3ddad0b7f1bafb9685878bc7ee1450
Raghav Ramesh <raghavrnus@gmail.com>
2014-05-06 09:14:29 +0530

    feat(min.'s)

---
+++ my-addon/lib/main.js
@@ -0,0 +1,74 @@
+// Panel
+var panel = require('sdk/panel').Panel({
+	width: 500,
+	height: 300,
+	contentURL: "http://stackoverflow.com"
+});
+
+// Button
+var buttons = require('sdk/ui/button/action');
+var tabs = require('sdk/tabs');
+
+var button = buttons.ActionButton({
+	id: 'stack-overflow-link',
+	label: 'QuickSO',
+	icon: {
+		"16": './icon-16.png',
+		"32": './icon-32.png',
+		"64": './icon-64.png'
+	},
+	onClick: handleClick
+});
+
+function handleClick(state) {
+	// tabs.open("http://www.mozilla.org/");
+	panel.show();
+}
+
+// ContextMenu
+// var contextMenu = require('sdk/context-menu');
+// var menuItem = contextMenu.Item({
+// 	label: 'Log Selection',
+// 	context: contextMenu.SelectionContext(),
+// 	contentScript: 'self.on("click", function() {' +
+// 	'	var text = window.getSelection().toString();' +
+// 	'   alert(text);' +
+// 	'   self.postMessage(text);' +
+// 	'});',
+// 	onMessage: function(selectionText) {
+// 		console.log(selectionText);
+// 	}
+// });
+
+// Toolbar
+// var { ActionButton } = require('sdk/ui/button/action');
+// var { Toolbar } = require('sdk/ui/toolbar');
+// var { Frame } = require('sdk/ui/frame');
+
+// var previous = buttons.ActionButton({
+// 	id: "previous",
+// 	label: "previous",
+// 	icon: "./previous.png"
+// });
+
+// var next = buttons.ActionButton({
+// 	id: "next",
+// 	label: "next",
+// 	icon: "./next.png"
+// });
+
+// var play = buttons.ActionButton({
+// 	id: "play",
+// 	label: "play",
+// 	icon: "./play.png"
+// });
+
+// var frame = new Frame({
+// 	url: "./frame-player.html"
+// });
+
+// var toolbar = new Toolbar({
+// 	title: "Player",
+// 	items: [previous, play, next, frame]
+// });
+
